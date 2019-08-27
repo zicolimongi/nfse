da82ac052ecbee000fea28070ce29217b222be61
ReinaldoACDC <reinaldoacdc@gmail.com>
2019-08-27 14:58:07 -0300

    remove files

--- teste.rb
+++
@@ -1,6 +0,0 @@
-require 'nfse_gem'
-
-wsdl = 'http://servicosweb.itabira.mg.gov.br:90/NFSe.Portal.Integracao.Teste/Services.svc?singleWsdl'
-consulta_lote = Nfse::ConsultaLote.new('3131703', '12345678901234', '12345678', '111111')
-
-puts consulta_lote.consultar()
