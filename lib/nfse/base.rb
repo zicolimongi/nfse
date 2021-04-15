b71487ab1ed8e414c94126b33bf43f2eea11ab8c
Willian <willian-varela@hotmail.com>
2021-04-15 12:20:35 -0300

    Ajuste prod link

--- lib/nfse/base.rb
+++ lib/nfse/base.rb
@@ -7,7 +7,7 @@ module Nfse
         def get_wsdl
             {
                 '3131703' => 'http://servicosweb.itabira.mg.gov.br:90/nfse.portal.integracao/services.svc?Wsdl',                
-                '3106200' => 'https://bhisshomologa.pbh.gov.br/bhiss-ws/nfse?wsdl'
+                '3106200' => 'https://bhissdigital.pbh.gov.br/bhiss-ws/nfse?wsdl'
             }
         end
     end
