252713b778d963a238511723d208c423f6549f51
Francisco Limongi <francisco@cfaz.net>
2024-03-14 21:14:04 -0300

    alteração de url da nota

--- lib/nfse/base.rb
+++ lib/nfse/base.rb
@@ -7,8 +7,8 @@ module Nfse
         def get_wsdl
             if Rails.env.production?
                 {
-                    '3131703' => 'http://servicosweb.itabira.mg.gov.br:90/nfse.portal.integracao/services.svc?Wsdl',                
-                    '3106200' => 'https://bhissdigital.pbh.gov.br/bhiss-ws/nfse?wsdl'
+                    '3131703' => 'http://servicosweb.itabira.mg.gov.br:90/nfse.portal.integracao/services.svc?Wsdl',
+                    '3106200' => 'https://bhissdigitalws.pbh.gov.br/bhiss-ws/nfse?wsdl'
                 }
             else
                 {
